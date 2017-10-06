afb9f673a188067e11f987199161615ff04e9c95
Hubert Asamer <hubert.asamer@dlr.de>
2017-10-06 12:09:33 +0200

    typo

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ BUILD=$(git rev-parse --short HEAD)
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-docker build -t asamerh4/sentinel3-tu-utm:$BUILD .
+docker build -t asamerh4/sentinel3-to-utm:$BUILD .
 
-echo -e ${YELLOW}"**build finished -> asamerh4/sentinel3-tu-utm:$BUILD"${NC}
+echo -e ${YELLOW}"**build finished -> asamerh4/sentinel3-to-utm:$BUILD"${NC}
